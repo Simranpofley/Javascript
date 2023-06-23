40db58ae10212688c7e108343ebf433b41afc9c8
Simranpofley <120714389+Simranpofley@users.noreply.github.com>
2023-06-23 08:54:02 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,50 @@
+let x= 10
+console.log(x) 
+let d= 9000
+console.log(d)
+
+const y=200
+console.log(y)
+
+let n=20
+let s=3
+console.log(n+s)
+console.log(n-s)
+console.log(n*s)
+console.log(n/s)
+console.log(n%s)
+
+let q=10
+let w=5
+console.log(q+w)
+console.log(q-w)
+console.log(q*w)
+console.log(q/w)
+console.log(q%w)
+
+
+
+
+let h=39
+console.log(h)
+
+let j=800
+let k=30
+console.log(j+k)
+console.log(j-k)
+console.log(j*k)
+console.log(j/k)
+console.log(j%k)
+
+function Calculator (x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+
+}
+
+Calculator(12,6)
+
+Calculator(600,700)
